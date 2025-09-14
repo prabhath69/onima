708ec9d2a522c82951d2b0a370701f3d307f730b
prabhath69 <prabhub480@gmail.com>
2025-09-15 00:28:48 +0530

    Added Chatbot

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,44 @@ import Story from './pages/Story';
 import Services from './pages/Services';
 import Contact from './pages/Contact';
 import Footer from './components/Footer';
+// App.tsx
+import { useEffect } from 'react';
+import '@n8n/chat/style.css';
+import { createChat } from '@n8n/chat';
+import './chatbot-styles.css'
 
 function App() {
+  useEffect(() => {
+		createChat({
+			webhookUrl: 'http://n8n.ofzen.in:5678/webhook/22f03920-3d1b-40ab-9259-38293f22aa97/chat',
+      webhookConfig: {
+		method: 'POST',
+		headers: {}
+	},
+	target: '#n8n-chat',
+	mode: 'window',
+	chatInputKey: 'chatInput',
+	chatSessionKey: 'sessionId',
+	loadPreviousSession: true,
+	metadata: {},
+	showWelcomeScreen: false,
+	defaultLanguage: 'en',
+	initialMessages: [
+		'Hello!',
+		'My name is Alex. What brings you to Onima?'
+	],
+	i18n: {
+		en: {
+			title: 'Hi',
+			subtitle: "Welcome to Onima!",
+			footer: '',
+			getStarted: 'New Conversation',
+			inputPlaceholder: 'Type your question..',
+		},
+	},
+	enableStreaming: false,
+		});
+	}, []);
   return (
     <Router>
       <div className="bg-neutral-950 text-white overflow-x-hidden">
